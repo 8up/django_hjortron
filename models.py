b12ecba5c62e5d578d8ba9a1498179f252adb0f8
Erik Ylipää <erik.ylipaa@gmail.com>
2011-05-15 11:06:26 +0200

    Initial commit

---
+++ models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    def __init__(self):
+        """ Bah
+        """
+
+class SubBoard(model.Model):
+    name
+    color
+    position_x
+    position_y
+    width
+    height
+    
+
+    def __init__(self):
+        """
+        """
+
+class Note(model.Model):
+    def __init__(self):
+        """Foo
+        """ 
+
+
+
+class Avatar(model.Model):
+    
+
+    
